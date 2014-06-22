43ee1c9ae1ba7d43d744baf83803e77b64e3b3fb
Steve McMahon <steve@dcn.org>
2014-06-21 21:38:10 -0500

    Add whipdialog

---
+++ helper_scripts/whipdialog.sh
@@ -0,0 +1,109 @@
+#!/bin/sh
+# Wrapper for whiptail/dialog/bash-console to display the interface on console
+
+WHIPTAIL () {
+    height=20
+    width=78
+    dtype=error
+    scrolltext=""
+    title=""
+    for option; do
+        optarg=`expr "x$option" : 'x[^=]*=\(.*\)'`
+        case $option in
+            --height=*)
+                height=$optarg
+                ;;
+            --width=*)
+                width=$optarg
+                ;;
+            --yesno | --msgbox | --inputbox | --passwordbox | --menu )
+                dtype=$option
+                ;;
+            --title=* )
+                title="$optarg"
+                ;;
+            --scrolltext )
+                scrolltext=$option
+                ;;
+            --choices )
+                choices="$optarg"
+                ;;
+            *)
+                prompt="$option"
+                ;;
+        esac
+    done
+
+
+    if [ "X$whipdialog" == "X" ]; then
+        whipdialog=`which dialog || which whiptail`
+    	if [ $? -gt 0 ]; then
+    	    whipdialog="bashme"
+    	fi
+    fi
+    if [ "X$whipdialog" == "Xdialog" ]; then
+        scrolltext=""
+    fi
+
+    if [ $whipdialog == "bashme" ]; then
+        echo
+        echo "=========================================="
+        echo "$title"
+        echo "=========================================="
+        case "$dtype" in
+            --yesno)
+                echo "$prompt"
+                echo
+                select answer in "Yes" "No"; do
+                    case $answer in
+                        "Yes")
+                            [ 0 -eq 0 ]
+                            return "$?"
+                            ;;
+                        "No")
+                            [ 1 -eq 0 ]
+                            return "$?"
+                            ;;
+                    esac
+                done
+                ;;
+            --menu)
+                echo $prompt
+                select answer in "${MENU_CHOICES[@]}"; do
+                    WHIPTAIL_RESULT="$answer"
+                    break
+                done
+                ;;
+            --msgbox)
+                echo "$prompt"
+                echo
+                read -p "Press any key: " -n 1
+                echo
+                ;;
+            --inputbox)
+                read -e -p "$prompt" WHIPTAIL_RESULT
+                ;;
+            --passwordbox)
+                read -e -s -p "$prompt" WHIPTAIL_RESULT
+                ;;
+            *)
+                echo "Unknown dialog type"
+                exit 1
+        esac
+    else
+        if [ "$dtype" == "--menu" ]; then
+            # double the choices
+            DCHOICES=()
+            for item in "${MENU_CHOICES[@]}"; do
+                DCHOICES=("${DCHOICES[@]}" "$item" "")
+            done
+            WHIPTAIL_RESULT=$($whipdialog --title "$title" \
+                $dtype "$prompt" $height $width \
+                ${#MENU_CHOICES[@]} "${DCHOICES[@]}" 3>&1 1>&2 2>&3)
+        else
+            WHIPTAIL_RESULT=$($whipdialog --title "$title" $dtype "$prompt" \
+                $height $width 3>&1 1>&2 2>&3)
+        fi
+    fi
+    return $?
+}
---
+++ helper_scripts/whipdialog_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+. whipdialog.sh
+
+WHIPTAIL --title="Yes or No" --yesno "Make a decision, yes or no."
+DECISION=$?
+
+WHIPTAIL --title="Alert box" --msgbox "You should know..."
+
+WHIPTAIL --title="Input Box" --inputbox "Tell me something new."
+INBOX=$WHIPTAIL_RESULT
+
+WHIPTAIL --title="Password Box" --passwordbox "Tell me something private."
+PRIVATE=$WHIPTAIL_RESULT
+
+MENU_CHOICES=("One" "Two 2" "Three")
+WHIPTAIL --title="Menu" --menu "Draw me a menu"
+MENU=$WHIPTAIL_RESULT
+
+echo
+echo $DECISION
+echo $INBOX
+echo $PRIVATE
+echo $MENU
+
