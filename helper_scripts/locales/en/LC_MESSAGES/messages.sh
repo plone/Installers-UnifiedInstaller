38cde2e0ee6a7112ae83c52021857b6e1656b43c
Steve McMahon <steve@dcn.org>
2014-07-13 14:29:30 -0700

    quoting error fix

--- helper_scripts/locales/en/LC_MESSAGES/messages.sh
+++ helper_scripts/locales/en/LC_MESSAGES/messages.sh
@@ -354,7 +354,7 @@ ADD_USER_TO_GROUP='Adding user $TARGET_USER to group $TARGET_GROUP.'
 
 USING_PW="Using pw to create users and groups"
 
-UNKNOWN_USER_ENV='We don't know how to add users and groups in this environment.
+UNKNOWN_USER_ENV='We do not know how to add users and groups in this environment.
 Please add users and group manually, then try again.
 See README.txt for instructions on how to proceed.
 '
