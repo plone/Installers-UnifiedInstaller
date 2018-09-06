5632c63d8669245d6b5aef4bb4f8900e19fd70d3
Steve McMahon <steve@dcn.org>
2018-09-06 10:44:03 -0700

    checkpoint

--- helper_scripts/windows_install.py
+++ helper_scripts/windows_install.py
@@ -7,9 +7,15 @@
 # We're starting with the assumption of a working Python and the MS VCC
 # for Python kit.
 
-import argparse
+
 from i18n import _
 
+import argparse
+import os
+import os.path
+
+
+PLONE_HOME = os.path.join(os.environ['HOMEPATH'], 'Plone')
 
 ##########################################################
 # Get command line arguments
@@ -21,5 +27,19 @@ argparser.add_argument(
     choices=('zeo', 'standalone'),
     help=_("Instance type to create."),
 )
+
+argparser.add_argument(
+    '--password',
+    required=False,
+    help=_("Instance password; If not specified, a random password will be generated.")
+)
+argparser.add_argument(
+    "--target",
+    default=PLONE_HOME,
+    help="Use to specify top-level path for installs."
+         "Plone instances will be built inside this directory."
+         "(default is {}).".format(PLONE_HOME),
+)
+
 
 opt = argparser.parse_args()
